73feb6b06f17c5440ae9d4be342c8f8d2319d207
Harriet <harriet.ilsley@gmail.com>
2025-09-14 19:18:12 +0100

    Refactor Piece struct and enhance main loop for movement and rotation

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,19 @@ use macroquad::prelude::*;
 pub struct Piece {
     circle_positions: Vec<[u32; 2]>,
     centre_of_rotation: [u32; 2],
+    circle_radius: f32,
+    position: Vec2,
+    rotation: f32,
     colour: macroquad::color::Color,
     render_target: macroquad::texture::RenderTarget,
     render_camera: macroquad::camera::Camera2D,
 }
 
 impl Piece {
-    pub fn new(circle_positions: Vec<[u32; 2]>, centre_of_rotation: [u32; 2], colour: macroquad::color::Color) -> Piece {
+    pub fn new(circle_positions: Vec<[u32; 2]>, centre_of_rotation: [u32; 2], circle_radius: f32, grid_pos: [u32; 2], top_left_pos: Vec2, colour: macroquad::color::Color) -> Piece {
+        let position = Vec2::new(top_left_pos.x + grid_pos[0] as f32 * circle_radius * 2.0, top_left_pos.y + grid_pos[1] as f32 * circle_radius * 2.0);
         let render_target = render_target(256, 256);
-        render_target.texture.set_filter(FilterMode::Nearest);
+        render_target.texture.set_filter(FilterMode::Linear);
         let render_camera = Camera2D {
             render_target: Some(render_target.clone()),
             ..Camera2D::from_display_rect(Rect::new(0.0, 0.0, 256.0, 256.0))
@@ -20,6 +24,9 @@ impl Piece {
         Piece {
             circle_positions,
             centre_of_rotation,
+            circle_radius,
+            position,
+            rotation: 0.0,
             colour,
             render_target,
             render_camera,
@@ -30,23 +37,41 @@ impl Piece {
         set_camera(&self.render_camera);
 
         for pos in &self.circle_positions {
-            draw_circle(((pos[0] + 1) * 40) as f32, ((pos[1] + 1) * 40) as f32,  20.0, self.colour);
-            println!("TEST {}, {}", pos[0], pos[1]);
+            draw_circle((pos[0] as f32 + 0.5) * self.circle_radius * 2.0, self.render_target.texture.height() - (pos[1] as f32 + 0.5) * self.circle_radius * 2.0, self.circle_radius, self.colour);
         }
 
         set_default_camera();
     }
 
-    pub fn draw(&self, position: Vec2) {
+    pub fn draw(&self) {
         draw_texture_ex(
             &self.render_target.texture,
-            position.x,
-            position.y,
+            self.position.x,
+            self.position.y,
             WHITE,
             DrawTextureParams {
                 dest_size: Some(Vec2::new(256.0, 256.0)),
+                rotation: self.rotation.to_radians(),
+                pivot: Some(Vec2::new(self.position.x + (self.centre_of_rotation[0] * 2 + 1) as f32 * self.circle_radius, self.position.y +  (self.centre_of_rotation[1] * 2 + 1) as f32 * self.circle_radius)),
                 ..Default::default()
             }
         );
+    }
+
+    pub fn translate(&mut self, x: i32, y: i32) {
+        self.position.x += x as f32 * self.circle_radius * 2.0;
+        self.position.y += y as f32 * self.circle_radius * 2.0;
+    }
+
+    pub fn rotate(&mut self, clockwise: bool) {
+        self.rotation += if clockwise { 90.0 } else { -90.0 };
+    }
+}
+
+pub fn draw_circle_grid(x: f32, y: f32, rows: u32, cols: u32, circle_radius: f32, colour: macroquad::color::Color) {
+    for i in 0..cols {
+        for j in 0..rows {
+            draw_circle(x + (i as f32 + 0.5) * circle_radius * 2.0, y + (j as f32 + 0.5) * circle_radius * 2.0, circle_radius, colour);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,36 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let test = polyomino::Piece::new(vec![[0, 0], [0, 1], [1, 0]], [0, 0], RED);
+    let mut test = polyomino::Piece::new(vec![[0, 0], [0, 1], [1, 0]], [0, 0], 20.0, [0, 0], Vec2::new(screen_width() / 2.0 - 220.0, screen_height() / 2.0 - 100.0), RED);
     test.setup_texture();
 
     loop {
         clear_background(BLACK);
 
-        draw_rectangle_lines(screen_width() / 2.0 - 300.0, screen_height() / 2.0 - 200.0, 600.0, 400.0, 3.0, GRAY);
+        draw_rectangle_lines(screen_width() / 2.0 - 225.0, screen_height() / 2.0 - 105.0, 450.0, 210.0, 10.0, GRAY);
 
-        test.draw(Vec2::new(200.0, 200.0));
+        polyomino::draw_circle_grid(screen_width() / 2.0 - 220.0, screen_height() / 2.0 - 100.0, 5, 11, 20.0, Color::from_hex(0x2b2b2b));
+
+        test.draw();
+
+        if is_key_pressed(KeyCode::Right) {
+            test.translate(1, 0);
+        }
+        if is_key_pressed(KeyCode::Left) {
+            test.translate(-1, 0);
+        }
+        if is_key_pressed(KeyCode::Up) {
+            test.translate(0, -1);
+        }
+        if is_key_pressed(KeyCode::Down) {
+            test.translate(0, 1);
+        }
+        if is_key_pressed(KeyCode::Z) {
+            test.rotate(true);
+        }
+        if is_key_pressed(KeyCode::X) {
+            test.rotate(false);
+        }
 
         next_frame().await
     }

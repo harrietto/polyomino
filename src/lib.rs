aa61beb1813627265c94f115c31c496f3a9d843f
Harriet <harriet.ilsley@gmail.com>
2025-09-20 20:54:37 +0100

    Refactor Piece struct for improved data types and enhance main loop for piece management

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,21 @@ pub const BOARD_DIMENSIONS: [u32; 2] = [11, 5];
 
 pub struct Piece {
     circle_positions: Vec<[u32; 2]>,
-    centre_of_rotation: [u32; 2],
+    centre_of_rotation: [i32; 2],
     circle_radius: f32,
-    position: Vec2,
-    rotation: f32,
+    top_left_pos: Vec2,
+    position: [i32; 2],
+    rotation: i32,
     locked: bool,
     colour: macroquad::color::Color,
     render_target: macroquad::texture::RenderTarget,
     render_camera: macroquad::camera::Camera2D,
+    index: usize,
 }
 
 impl Piece {
-    pub fn new(circle_positions: Vec<[u32; 2]>, centre_of_rotation: [u32; 2], circle_radius: f32, grid_pos: [u32; 2], top_left_pos: Vec2, colour: macroquad::color::Color) -> Piece {
-        let position = Vec2::new(top_left_pos.x + grid_pos[0] as f32 * circle_radius * 2.0, top_left_pos.y + grid_pos[1] as f32 * circle_radius * 2.0);
+    pub fn new(index: usize, circle_positions: Vec<[u32; 2]>, centre_of_rotation: [i32; 2], circle_radius: f32, grid_pos: [i32; 2], top_left_pos: Vec2, colour: macroquad::color::Color) -> Piece {
+        let position = [grid_pos[0] - centre_of_rotation[0], grid_pos[1] - centre_of_rotation[1]];
         let render_target = render_target(256, 256);
         render_target.texture.set_filter(FilterMode::Linear);
         let render_camera = Camera2D {
@@ -28,12 +30,14 @@ impl Piece {
             circle_positions,
             centre_of_rotation,
             circle_radius,
+            top_left_pos,
             position,
-            rotation: 0.0,
+            rotation: 0,
             colour,
             locked: false,
             render_target,
             render_camera,
+            index,
         }
     }
 
@@ -50,31 +54,33 @@ impl Piece {
     pub fn draw(&self) {
         draw_texture_ex(
             &self.render_target.texture,
-            self.position.x,
-            self.position.y,
-            WHITE,
+            self.top_left_pos.x + self.position[0] as f32 * self.circle_radius * 2.0,
+            self.top_left_pos.y + self.position[1] as f32 * self.circle_radius * 2.0,
+            if self.locked { self.colour } else { Color::from_hex(0x808080) },
             DrawTextureParams {
                 dest_size: Some(Vec2::new(256.0, 256.0)),
-                rotation: self.rotation.to_radians(),
-                pivot: Some(Vec2::new(self.position.x + (self.centre_of_rotation[0] * 2 + 1) as f32 * self.circle_radius, self.position.y +  (self.centre_of_rotation[1] * 2 + 1) as f32 * self.circle_radius)),
+                rotation: (self.rotation as f32).to_radians(),
+                pivot: Some(self.top_left_pos + Vec2::new(((self.position[0] + self.centre_of_rotation[0]) * 2 + 1) as f32 * self.circle_radius, ((self.position[1] + self.centre_of_rotation[1]) * 2 + 1) as f32 * self.circle_radius)),
                 ..Default::default()
             }
         );
     }
 
-    pub fn translate(&mut self, x: i32, y: i32, top_left_pos: Vec2, bottom_right_pos: Vec2) {
+    pub fn translate(&mut self, x: i32, y: i32) {
         if self.locked == true {
             return;
         }
 
-        let new_centre_of_rotation_coord = Vec2::new(self.position.x + ((self.centre_of_rotation[0] as i32 + x) * 2 + 1) as f32 * self.circle_radius, self.position.y +  ((self.centre_of_rotation[1] as i32 + y) * 2 + 1) as f32 * self.circle_radius);
-
-        if new_centre_of_rotation_coord.x < top_left_pos.x || new_centre_of_rotation_coord.x > bottom_right_pos.x || new_centre_of_rotation_coord.y < top_left_pos.y || new_centre_of_rotation_coord.y > bottom_right_pos.y {
+        if self.position[0] + self.centre_of_rotation[0] + x < 0 || self.position[0] + self.centre_of_rotation[0] + x > BOARD_DIMENSIONS[0] as i32 - 1 || self.position[1] + self.centre_of_rotation[1] + y < 0 || self.position[1] + self.centre_of_rotation[1] + y > BOARD_DIMENSIONS[1] as i32 - 1 {
             return;
         }
 
-        self.position.x += x as f32 * self.circle_radius * 2.0;
-        self.position.y += y as f32 * self.circle_radius * 2.0;
+        self.position[0] = self.position[0] + x;
+        self.position[1] = self.position[1] + y;
+    }
+
+    pub fn get_pos(&self) -> [i32; 2] {
+        [self.position[0] + self.centre_of_rotation[0], self.position[1] + self.centre_of_rotation[1]]
     }
 
     pub fn rotate(&mut self, clockwise: bool) {
@@ -82,49 +88,85 @@ impl Piece {
             return;
         }
 
-        self.rotation += if clockwise { 90.0 } else { -90.0 };
+        self.rotation += if clockwise { 90 } else { -90 };
     }
 
-    pub fn lock(&mut self, spaces: &mut [[Option<&Piece>; BOARD_DIMENSIONS[0] as usize]; BOARD_DIMENSIONS[1] as usize]) {
+    fn get_rotated_pos(&self, pos: &[u32; 2]) -> [i32; 2] {
+        match self.rotation % 360 {
+            0 => [self.position[0] + pos[0] as i32, self.position[1] + pos[1] as i32],
+            90 | -270 => [self.position[0] - pos[1] as i32, self.position[1] + pos[0] as i32],
+            180 | -180 => [self.position[0] - pos[0] as i32, self.position[1] - pos[1] as i32],
+            270 | -90 => [self.position[0] + pos[1] as i32, self.position[1] - pos[0] as i32],
+            _ => panic!("Rotation not a multiple of 90 degrees"),
+        }
+    }
+
+    pub fn lock(&mut self, spaces: &mut [[Option<usize>; BOARD_DIMENSIONS[0] as usize]; BOARD_DIMENSIONS[1] as usize]) -> Result<(), ()> {
+        for pos in &self.circle_positions {
+            let circle_pos = self.get_rotated_pos(&pos);
+
+            if circle_pos[0] < 0 || circle_pos[1] < 0 || circle_pos[0] > BOARD_DIMENSIONS[0] as i32 - 1 || circle_pos[1] > BOARD_DIMENSIONS[1] as i32 - 1 || spaces[circle_pos[1] as usize][circle_pos[0] as usize] != None {
+                return Err(());
+            }
+        }
+
+        for pos in &self.circle_positions {
+            let circle_pos = self.get_rotated_pos(&pos);
+
+            spaces[circle_pos[1] as usize][circle_pos[0] as usize] = Some(self.index);
+        }
+
         self.locked = true;
+
+        Ok(())
+    }
+
+    pub fn unlock(&mut self, spaces: &mut [[Option<usize>; BOARD_DIMENSIONS[0] as usize]; BOARD_DIMENSIONS[1] as usize]) {
+        for pos in &self.circle_positions {
+            let circle_pos = self.get_rotated_pos(&pos);
+
+            spaces[circle_pos[1] as usize][circle_pos[0] as usize] = None;
+        }
+
+        self.locked = false;
     }
 }
 
 pub struct Cursor {
-    position: Vec2,
+    position: [i32; 2],
     circle_radius: f32,
     top_left_pos: Vec2,
-    bottom_right_pos: Vec2,
 }
 
 impl Cursor {
-    pub fn new(position: [u32; 2], circle_radius: f32, top_left_pos: Vec2, bottom_right_pos: Vec2) -> Cursor {
-        let position = Vec2::new(top_left_pos.x + (position[0] * 2 + 1) as f32 * circle_radius, top_left_pos.y + (position[1] * 2 + 1) as f32 * circle_radius);
-
+    pub fn new(position: [i32; 2], circle_radius: f32, top_left_pos: Vec2) -> Cursor {
         Cursor {
             position,
             circle_radius,
             top_left_pos,
-            bottom_right_pos,
         }
     }
 
-    pub fn set_pos(&mut self, position: [u32; 2]) {
-        self.position = Vec2::new(self.top_left_pos.x + (position[0] * 2 + 1) as f32 * self.circle_radius, self.top_left_pos.y + (position[1] * 2 + 1) as f32 * self.circle_radius);
+    pub fn set_pos(&mut self, position: [i32; 2]) {
+        self.position = position;
     }
 
+    pub fn get_pos(&self) -> [i32; 2] {
+        self.position
+    }
+ 
     pub fn translate(&mut self, change: [i32; 2]) {
-        let new_position = Vec2::new(self.position.x + change[0] as f32 * self.circle_radius * 2.0, self.position.y + change[1] as f32 * self.circle_radius * 2.0);
-
-        if new_position.x < self.top_left_pos.x || new_position.x > self.bottom_right_pos.x || new_position.y < self.top_left_pos.y || new_position.y > self.bottom_right_pos.y {
+        if self.position[0] + change[0] < 0 || self.position[0] + change[0] > BOARD_DIMENSIONS[0] as i32 - 1 || self.position[1] + change[1] < 0 || self.position[1] + change[1] > BOARD_DIMENSIONS[1] as i32 - 1 {
             return;
         }
 
-        self.position = new_position;
+        self.position = [self.position[0] + change[0], self.position[1] + change[1]];
     }
 
     pub fn draw(&self) {
-        draw_circle_lines(self.position.x, self.position.y, self.circle_radius - 3.0, 3.0, WHITE);
+        let draw_pos = Vec2::new(self.top_left_pos.x + (self.position[0] as f32 + 0.5) * self.circle_radius * 2.0, self.top_left_pos.y + (self.position[1] as f32 + 0.5) * self.circle_radius * 2.0);
+
+        draw_circle_lines(draw_pos.x, draw_pos.y, self.circle_radius - 3.0, 3.0, WHITE);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,22 @@ async fn main() {
     let circle_radius = 20.0;
     let board_size = Vec2::new((polyomino::BOARD_DIMENSIONS[0]) as f32 * circle_radius * 2.0, (polyomino::BOARD_DIMENSIONS[1]) as f32 * circle_radius * 2.0);
     let outline_size = Vec2::new((polyomino::BOARD_DIMENSIONS[0] + 2) as f32 * circle_radius * 2.0, (polyomino::BOARD_DIMENSIONS[1] + 2) as f32 * circle_radius * 2.0);
-    let mut cursor = polyomino::Cursor::new([0, 0], circle_radius, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), Vec2::new(screen_width() / 2.0 + board_size.x / 2.0, screen_height() / 2.0 + board_size.y / 2.0));
-    let mut test = polyomino::Piece::new(vec![[0, 0], [0, 1], [1, 0]], [0, 0], circle_radius, [0, 0], Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), RED);
-    test.setup_texture();
-    let mut spaces: [[Option<&polyomino::Piece>; polyomino::BOARD_DIMENSIONS[0] as usize]; polyomino::BOARD_DIMENSIONS[1] as usize] = [[None; polyomino::BOARD_DIMENSIONS[0] as usize]; polyomino::BOARD_DIMENSIONS[1] as usize];
+
+    let mut cursor = polyomino::Cursor::new([0, 0], circle_radius, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0));
+
+    let red = polyomino::Piece::new(0, vec![[0, 1], [0, 2], [1, 1], [1, 0]], [0, 1], circle_radius, [0, 1], Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), RED);
+    red.setup_texture();
+    let blue = polyomino::Piece::new(1, vec![[0, 0], [0, 1], [0, 2], [1, 0]], [0, 0], circle_radius, [6, 2], Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), BLUE);
+    blue.setup_texture();
+
+    let mut pieces = [red, blue];
+    let mut spaces: [[Option<usize>; polyomino::BOARD_DIMENSIONS[0] as usize]; polyomino::BOARD_DIMENSIONS[1] as usize] = [[None; polyomino::BOARD_DIMENSIONS[0] as usize]; polyomino::BOARD_DIMENSIONS[1] as usize];
+    let mut movable_piece: Option<usize> = Some(0);
+    if let Some(index) = movable_piece {
+        cursor.set_pos(pieces[index].get_pos());
+    }
+
+    pieces[1].lock(&mut spaces).unwrap();
 
     loop {
         clear_background(BLACK);
@@ -28,34 +40,61 @@ async fn main() {
 
         polyomino::draw_circle_grid(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0, polyomino::BOARD_DIMENSIONS[1], polyomino::BOARD_DIMENSIONS[0], circle_radius, Color::from_hex(0x2b2b2b));
 
-        test.draw();
+        for piece in &pieces {
+            piece.draw();
+        }
         cursor.draw();
 
         if is_key_pressed(KeyCode::Right) {
             cursor.translate([1, 0]);
-            test.translate(1, 0, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), Vec2::new(screen_width() / 2.0 + board_size.x / 2.0, screen_height() / 2.0 + board_size.y / 2.0));
+            if let Some(index) = movable_piece {
+                pieces[index].translate(1, 0);
+            }
         }
         if is_key_pressed(KeyCode::Left) {
             cursor.translate([-1, 0]);
-            test.translate(-1, 0, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), Vec2::new(screen_width() / 2.0 + board_size.x / 2.0, screen_height() / 2.0 + board_size.y / 2.0));
+            if let Some(index) = movable_piece {
+                pieces[index].translate(-1, 0);
+                
+            }
         }
         if is_key_pressed(KeyCode::Up) {
             cursor.translate([0, -1]);
-            test.translate(0, -1, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), Vec2::new(screen_width() / 2.0 + board_size.x / 2.0, screen_height() / 2.0 + board_size.y / 2.0));
+            if let Some(index) = movable_piece {
+                pieces[index].translate(0, -1);
+            }
         }
         if is_key_pressed(KeyCode::Down) {
             cursor.translate([0, 1]);
-            test.translate(0, 1, Vec2::new(screen_width() / 2.0 - board_size.x / 2.0, screen_height() / 2.0 - board_size.y / 2.0), Vec2::new(screen_width() / 2.0 + board_size.x / 2.0, screen_height() / 2.0 + board_size.y / 2.0));
+            if let Some(index) = movable_piece {
+                pieces[index].translate(0, 1);
+            }
         }
         if is_key_pressed(KeyCode::X) {
-            test.rotate(true);
+            if let Some(index) = movable_piece {
+                pieces[index].rotate(true);
+            }
         }
         if is_key_pressed(KeyCode::Z) {
-            test.rotate(false);
+            if let Some(index) = movable_piece {
+                pieces[index].rotate(false);
+            }
         }
         if is_key_pressed(KeyCode::Enter) {
-            test.lock(&mut spaces);
+            if let Some(index) = movable_piece {
+                match pieces[index].lock(&mut spaces) {
+                    Ok(()) => movable_piece = None,
+                    Err(()) => (),
+                };
+            } else if movable_piece == None {
+                if let Some(index) = spaces[cursor.get_pos()[1] as usize][cursor.get_pos()[0] as usize] {
+                    movable_piece = Some(index);
+                    pieces[index].unlock(&mut spaces);
+                    cursor.set_pos(pieces[index].get_pos());
+                }
+            }
         }
+
 
         next_frame().await
     }
